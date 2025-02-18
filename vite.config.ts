6fcbbeb129f03a16802ec3ddf958d050e6f8f699
JinWon <dlfjs9514@nate.com>
2025-02-19 00:25:03 +0900

    Chore : 빌드 오류로 인해 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // crypto 모듈을 crypto-browserify로 대체
+      crypto: 'crypto-browserify',
+    },
+  },
+  define: {
+    // process.env.NODE_ENV나 global 객체를 사용하려면 이렇게 설정해줍니다
+    global: {},
+  },
 })

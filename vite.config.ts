67d97afb74b5909f3aae61b2af1857e23f7cae48
JinWon <dlfjs9514@nate.com>
2025-02-19 00:42:51 +0900

    Fix : depoly error

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      crypto: 'crypto-browserify',
-      stream: 'stream-browserify',
-      assert: 'assert',
-      path: 'path-browserify',
-      fs: 'fs-web',
-    },
-  },
-  define: {
-    global: 'window', // 'global'을 'window'로 설정
-    process: {
-      env: {
-        NODE_ENV: '"production"',
-      },
-    },
-  },
 })
